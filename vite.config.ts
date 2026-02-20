05b09450f6ecbf326cf52741546c429ad1d871cc
GrandsonfrmO <109824631+GrandsonfrmO@users.noreply.github.com>
2026-02-20 20:41:01 +0000

    Fix build: use esbuild instead of terser for minification

--- vite.config.ts
+++ vite.config.ts
@@ -21,12 +21,7 @@ export default defineConfig(({ mode }) => {
       },
       build: {
         target: 'esnext',
-        minify: 'terser',
-        terserOptions: {
-          compress: {
-            drop_console: true,
-          },
-        },
+        minify: 'esbuild',
         rollupOptions: {
           output: {
             manualChunks: {
